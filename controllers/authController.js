de81ce589e91f693e67fcbe9ffd3ca4b638cb7ca
chandrajeetyadav12 <chandrajeet.yadav@crowdapps.in>
2026-01-14 18:42:10 +0530

    update the usercontroller login for the admin

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,18 +4,35 @@ const generateToken = require("../utils/generateToken");
 // REGISTER USER / ADMIN
 exports.register = async (req, res) => {
   try {
-    const { name, email, password, role } = req.body;
+    const { name, email, password, adminSecret } = req.body;
 
     const exists = await User.findOne({ email });
     if (exists) {
       return res.status(400).json({ message: "User already exists" });
     }
+    let role = "user"; // DEFAULT
+        // ADMIN CREATION LOGIC (ONE TIME ONLY)
+    if (adminSecret) {
+      if (adminSecret !== process.env.ADMIN_SECRET) {
+        return res.status(403).json({ message: "Invalid admin secret" });
+      }
 
+      // Check if admin already exists
+      const adminExists = await User.findOne({ role: "admin" });
+      if (adminExists) {
+        return res
+          .status(403)
+          .json({ message: "Admin already exists" });
+      }
+
+      role = "admin";
+    }
+        // CREATE USER
     const user = await User.create({
       name,
       email,
       password,
-      role: role || "user",
+      role,
       image: req.file ? req.file.path : "",
     });
 
@@ -25,8 +42,27 @@ exports.register = async (req, res) => {
       email: user.email,
       role: user.role,
       image: user.image,
-      message: "Registration successful.",
+      message:
+        role === "admin"
+          ? "Admin registered successfully"
+          : "User registered successfully",
     });
+    // const user = await User.create({
+    //   name,
+    //   email,
+    //   password,
+    //   role: role || "user",
+    //   image: req.file ? req.file.path : "",
+    // });
+
+    // res.status(201).json({
+    //   _id: user._id,
+    //   name: user.name,
+    //   email: user.email,
+    //   role: user.role,
+    //   image: user.image,
+    //   message: "Registration successful.",
+    // });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
